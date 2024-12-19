b2ab31eb2f12776586f0b00461524058f501bcbe
paek1127 <paek1127@gmail.com>
2024-12-19 10:40:58 +0900

    dto jsonignore

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.ll.rest.domain.post.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ll.rest.domain.post.post.entity.Post;
 import lombok.Getter;
 
@@ -8,7 +9,9 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
+    @JsonIgnore
     private LocalDateTime createDate;
+    @JsonIgnore
     private LocalDateTime modifyDate;
     private String title;
     private String content;
@@ -19,5 +22,13 @@ public class PostDto {
         this.modifyDate = post.getModifyDate();
         this.title = post.getTitle();
         this.content = post.getContent();
+    }
+
+    public LocalDateTime getCreatedDatetime() {
+        return createDate;
+    }
+
+    public LocalDateTime getModifiedDatetime() {
+        return modifyDate;
     }
 }
