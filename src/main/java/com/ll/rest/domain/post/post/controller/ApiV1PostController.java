872afde9b3e5418d98661e93ff8a9157f2ea7c54
paek1127 <paek1127@gmail.com>
2025-01-08 20:29:30 +0900

    RsData클래스 추가, Map 대신 RsData return하기

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,15 +2,14 @@ package com.ll.rest.domain.post.post.controller;
 
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
+import com.ll.rest.global.rsData.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -29,15 +28,11 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public Map<String, Object> deleteItem(@PathVariable long id) {
+    public RsData deleteItem(@PathVariable long id) {
         Post post = postService.findById(id).get();
         postService.delete(post);
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 글 삭제 완료".formatted(id));
-
-        return rsData;
+        return new RsData("200-1", "%d번 글 삭제 완료".formatted(id));
     }
 
     @AllArgsConstructor
@@ -49,15 +44,11 @@ public class ApiV1PostController {
 
     @PutMapping("/{id}")
     @Transactional
-    public Map<String, Object> modifyItem(@PathVariable long id, @RequestBody PostModifyReqBody reqBody) {
+    public RsData modifyItem(@PathVariable long id, @RequestBody PostModifyReqBody reqBody) {
         Post post = postService.findById(id).get();
 
         postService.modify(post, reqBody.getTitle(), reqBody.getContent());
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 글 수정 완료".formatted(id));
-
-        return rsData;
+        return new RsData("200-1", "%d번 글 수정 완료".formatted(id));
     }
 }
