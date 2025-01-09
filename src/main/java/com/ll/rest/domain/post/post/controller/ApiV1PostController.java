d836dd8dc7db504d6b9aafe616d950d72041f634
paek1127 <paek1127@gmail.com>
2025-01-10 00:36:27 +0900

    json 데이터값이 null일때 JsonInclude 로 출력 안되게 처리

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -79,6 +79,6 @@ public class ApiV1PostController {
     public RsData writeItem(@RequestBody @Valid PostWriteReqBody reqBody) {
         Post post = postService.write(reqBody.title, reqBody.content);
 
-        return new RsData("200-1", "%d번 글 작성 완료".formatted(post.getId()));
+        return new RsData("200-1", "%d번 글 작성 완료".formatted(post.getId()), post.getId());
     }
 }
