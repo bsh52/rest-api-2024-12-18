dc47c60a7ce2d00adc4ef93d9ee2142bf45af090
paek1127 <paek1127@gmail.com>
2025-01-10 14:02:01 +0900

    post시 내용 없을때 에러 처리

--- src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.ll.rest.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,6 +21,20 @@ public class GlobalExceptionHandler {
                         new RsData<>(
                                 "404-1",
                                 "해당 데이터가 존재하지 않습니다."
+                        )
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RsData<Void>> handler(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        new RsData<>(
+                                "400-1",
+                                message
                         )
                 );
     }
